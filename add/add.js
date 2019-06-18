4df0c8d92f9e575a4de9d4c5e4575c005adaf61d
Youssef Hamid <yhamid1982@gmail.com>
2019-06-17 21:28:13 -0400

    updated Kyanite to 0.13 and added to add.js console log

--- add/add.js
+++ add/add.js
@@ -2,18 +2,19 @@ const { add, reduce } = require('kyanite')
 
 // Examples
 
-console.log(add(1, 1)) // => 2
+console.log('add(1,1) => ', add(1, 1)) // => 2
 
-console.log(add(1.5, 2.3)) // => 3.8
+console.log('add(1.5, 2.3) => ', add(1.5, 2.3)) // => 3.8
 
-console.log(add(7, -5)) // => 2
+console.log('add(7, -5) => ', add(7, -5)) // => 2
 
 // Curried Examples
+console.log('------------------Curried Examples------------------')
 
 const add7 = add(7)
 
-console.log(add7(3)) // => 10
+console.log('add7(3) => ', add7(3)) // => 10
 
-console.log(add7(54)) // => 61
+console.log('add7(54) => ', add7(54)) // => 61
 
-console.log(reduce(add, 0, [1, 2, 3])) // => 6
+console.log('reduce(add, 0, [1, 2, 3]) => ', reduce(add, 0, [1, 2, 3])) // => 6
