f61118813ceca7b0ec4a9875a38f0bec20810646
Youssef Hamid <yhamid1982@gmail.com>
2019-06-17 21:44:39 -0400

    updated concat

--- concat/concat.js
+++ concat/concat.js
@@ -2,8 +2,8 @@ const { concat, map } = require('kyanite')
 
 // Examples
 
-console.log(concat([4, 5, 6], [1, 2, 3])) // => [1, 2, 3, 4, 5, 6]
+console.log('concat([4, 5, 6], [1, 2, 3]) => ', concat([4, 5, 6], [1, 2, 3])) // => [1, 2, 3, 4, 5, 6]
 
-console.log(concat('ue', 'val')) // => 'value'
+console.log("concat('ue', 'val') => ", concat('ue', 'val')) // => 'value'
 
-console.log(map(concat('s'), 'hello')) // => ['hs', 'es', 'ls', 'ls', 'os']
+console.log("map(concat('s'), 'hello') => ", map(concat('s'), 'hello')) // => ['hs', 'es', 'ls', 'ls', 'os']
